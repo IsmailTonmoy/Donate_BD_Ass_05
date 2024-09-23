e6322abf06be0cc5c2348261420f043ebf6c9dea
IsmailTonmoy <tonmoyismailhossain@gmail.com>
2024-09-24 00:10:39 +0600

    Add utilities and feature js

--- js/donate.js
+++ js/donate.js
@@ -1,23 +1,52 @@
+//Donate for Flood at Noakhali
+
 document.getElementById('donate')
 .addEventListener('click', function (event){
     event.preventDefault();
 
-    const addMoney = document.getElementById('donate-amount').value;
-    const addMoneyNumber = parseFloat(addMoney);
-    const nDonate = document.getElementById('n-donate').innerText;
-    const nDonateN = parseFloat(nDonate);
-    const noaDonate = addMoneyNumber + nDonateN
+    const addDonateM = getValueById('donate-amount');
+    const nDonate = getTextValueById('n-donate');
+
+    const noaDonate = addDonateM + nDonate;
 
     document.getElementById('n-donate').innerText = noaDonate;
 
-    const tDonation = document.getElementById('total-donation').innerText;
-    const ntDonation = parseFloat(tDonation);
+    const donationT = getTextValueById('total-donation');
+    const totalDonation = addDonateM + donationT;
+    document.getElementById('total-donation').innerText = totalDonation;
 
-    const totalDo = addMoneyNumber + ntDonation;
 
-    document.getElementById('total-donation').innerText = totalDo;
+
+
+
+
+
+
+
+
+
+
+
+
+    // const addMoney = document.getElementById('donate-amount').value;
+    // const addMoneyNumber = parseFloat(addMoney);
+    // const nDonate = document.getElementById('n-donate').innerText;
+    // const nDonateN = parseFloat(nDonate);
+    // const noaDonate = addMoneyNumber + nDonateN
+
+    // document.getElementById('n-donate').innerText = noaDonate;
+
+    // const tDonation = document.getElementById('total-donation').innerText;
+    // const ntDonation = parseFloat(tDonation);
+
+    // const totalDo = addMoneyNumber + ntDonation;
+
+    // document.getElementById('total-donation').innerText = totalDo;
 
 
     
 })
+
+
+//Donate for Flood Relief in Feni
     
